69a22d19822d539e4ae26e8438bc069ba4d0688f
Nicholas Hoops <nicholas.hoops@oktopos.com>
2025-11-10 15:39:28 +0100

    initial commit

---
+++ app/db/orm_models.py
@@ -0,0 +1,21 @@
+# --- ORM-Models für SQLAlchemy bzw. Alembic ---
+# --- path: /app/db/orm_models.py ---
+
+from __future__ import annotations
+
+from sqlalchemy import String, Integer
+from sqlalchemy.ext.asyncio import AsyncAttrs
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
+
+class Base(DeclarativeBase, AsyncAttrs):
+    pass
+
+
+class Laptop(Base):
+    __tablename__ = "laptops"
+
+    id: Mapped[int] = mapped_column(primary_key=True,
+                                    autoincrement=True)
+    laptop_name: Mapped[str] = mapped_column(String(200),
+                                             nullable=False)
