7cf3672a7810bc977056f08a0ad32a5eca8aea38
Nicholas Hoops <nicholas.hoops@oktopos.com>
2025-11-17 18:39:35 +0100

    create_package_api hinzugefügt

--- app/api/admin_api.py
+++ app/api/admin_api.py
@@ -9,10 +9,12 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from uuid import UUID
 
 from app.core.deps import postgres_dep
-from app.pydantic_models.admin_models import ProductIn, ProductOut, DepartmentOut, DepartmentIn, ProductListOut
+from app.pydantic_models.admin_models import ProductIn, ProductOut, DepartmentOut, DepartmentIn, ProductListOut, \
+    PackageOut, PackageIn
 from app.service.admin_service import (create_product_service, get_product_by_id_service, create_department_service,
                                        get_department_by_id_service, get_all_products_service, delete_product_service,
-                                       get_products_by_type_service)
+                                       get_products_by_type_service, get_all_departments_service,
+                                       create_package_service)
 
 router = APIRouter(prefix="/admin", tags=["admin"])
 
@@ -41,12 +43,12 @@ async def create_product_api(payload: ProductIn,
 async def get_products_by_type_api(product_type: str,
                                session: AsyncSession = Depends(postgres_dep)
                                ) -> list[ProductOut]:
-    #try:
+    try:
         products = await get_products_by_type_service(session, product_type)
 
         return products
 
-    #except Exception:
+    except Exception:
         raise HTTPException(status_code=500, detail="Internal Error")
 
 
@@ -113,6 +115,17 @@ async def create_department_api(payload: DepartmentIn,
         raise HTTPException(status_code=500, detail="Internal Error")
 
 
+@router.get("/department", response_model=list[DepartmentOut])
+async def get_all_departments_api(session: AsyncSession = Depends(postgres_dep)):
+    try:
+        dto = await get_all_departments_service(session)
+
+        return dto
+
+    except Exception:
+        raise HTTPException(status_code=500, detail="Internal Error")
+
+
 @router.get("/department/{ident}", response_model=DepartmentOut, name="get_department_by_id",
             summary="Einzelne Abteilung anhand seiner ID abrufen.")
 async def get_department_by_id_api(ident: UUID,
@@ -131,7 +144,21 @@ async def get_department_by_id_api(ident: UUID,
 
 # --- Paketspezifische Endpunkte ---
 
+@router.post("/package", status_code=201, response_model=PackageOut)
+async def create_package_api(payload: PackageIn,
+                             request: Request,
+                             response : Response,
+                             session: AsyncSession = Depends(postgres_dep)):
+    try:
+        dto = await create_package_service(session, payload=payload)
 
+        location = str(request.url_for("get_department_by_id", ident=dto.id))
+        response.headers["Location"] = location
+
+        return dto
+
+    except Exception:
+        raise HTTPException(status_code=503, detail="Service Unavailable")
 
 
 # --- Tests ---
