39540f9016a1bf745249e5738cd906a163499c1a
Nicholas Hoops <nicholas.hoops@oktopos.com>
2025-11-19 14:22:15 +0100

    /calculator- und /home-Endpunkte hinzugefügt

--- app/api/user_api.py
+++ app/api/user_api.py
@@ -3,16 +3,45 @@
 
 from __future__ import annotations
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Form, Request, HTTPException
+from fastapi.templating import Jinja2Templates
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.core.deps import postgres_dep
-from app.pydantic_models.user_models import SpecsIn
+from app.pydantic_models.user_models import CalculatorIn, CalculatorOut
+from app.service.user_service import calculate_prices_service
 
-
+templates = Jinja2Templates(directory="templates")
 router = APIRouter(prefix="/user",tags=["user"])
 
-@router.post("/", summary="Anforderungen von Website erhalten.")
-async def get_user_specifications(payload: SpecsIn,
-                                  session: AsyncSession = Depends(postgres_dep)):
-    pass
+
+@router.post("/calculator", response_model=list[CalculatorOut], summary="Anforderungen von Website erhalten.")
+async def get_user_specifications(request: Request,
+                                  department: str = Form(...),
+                                  team_members: int = Form(...),
+                                  device_type: str = Form(...),
+                                  operating_system: str = Form(...),
+                                  included_software: str = Form(...),
+                                  session: AsyncSession = Depends(postgres_dep)
+                                  ) -> list[CalculatorOut]:
+    #try:
+        usage_mode = "mobile" if device_type.lower() == "laptop" else "stationary"
+
+        payload = CalculatorIn(department=department, team_members=team_members, usage_mode=usage_mode,
+                               os=operating_system, included_software=included_software)
+
+        dto = await calculate_prices_service(session, payload=payload)
+
+        return dto
+
+    #except ValueError:
+        raise HTTPException(status_code=404, detail="Not Found")
+    # Exception:
+        raise HTTPException(status_code=503, detail="Service Unavailable")
+
+
+
+
+@router.get("/home", summary="Landing Page")
+async def home_api(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
