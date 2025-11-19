6efbe9f83ef08d6124c7df6aa44873149ca46712
Nicholas Hoops <nicholas.hoops@oktopos.com>
2025-11-19 14:25:26 +0100

    get_department_by_name hinzugefügt

---
+++ app/db/repositories/user_repository.py
@@ -0,0 +1,24 @@
+# --- DB-Aufrufe für user Schicht ---
+# --- path: /app/db/repositories/user_repository.py ---
+
+from __future__ import annotations
+
+from sqlalchemy import select
+from sqlalchemy.orm import selectinload
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.db.orm_models import Department, PackageItem, Package
+
+
+async def get_department_by_name(session: AsyncSession, department_name: str) -> Department | None:
+    stmt = (select(Department)
+            .options(selectinload(Department.packages).
+                     selectinload(Package.items).
+                     selectinload(PackageItem.product))
+            .where(Department.name == department_name))
+
+    query = await session.execute(stmt)
+
+    department_orm = query.scalars().first()
+
+    return department_orm
