3d0c51fd94f1353aa34f45a4761323a01816452f
Nicholas Hoops <nicholas.hoops@oktopos.com>
2025-11-19 23:07:21 +0100

    StaticFiles mount auf app

--- app/main.py
+++ app/main.py
@@ -4,12 +4,17 @@
 from __future__ import annotations
 
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from pathlib import Path
 from uvicorn import run
 
 from app.api.admin_api import router as admin_router
 from app.api.user_api import router as user_router
 from app.core.lifespan import lifespan
 from app.core.settings import settings
+
+
+BASE_DIR = Path(__file__).resolve().parent
 
 app = FastAPI(debug=True,
               title=settings.APP_NAME,
@@ -19,8 +24,12 @@ app = FastAPI(debug=True,
               redoc_url=f"{settings.API_PREFIX}/redoc",
               lifespan=lifespan)
 
+
+app.mount("/static", StaticFiles(directory=str(BASE_DIR / "static")), name="static")
+
 app.include_router(admin_router)
 app.include_router(user_router)
+
 
 if __name__ == "__main__":
     run("app.main:app", host=settings.HOST_LOCAL, port=settings.PORT, reload=settings.RELOAD, log_level=settings.LOG_LEVEL.lower())
