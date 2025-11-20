0de610791e298eb2929c6873c3fff3ae397b7ba9
Nicholas Hoops <nicholas.hoops@oktopos.com>
2025-11-20 10:17:33 +0100

    überarbeitet

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,9 +1,13 @@
 document.getElementById("tryBtn").addEventListener("click", function () {
     const calculator = document.getElementById("calculator");
+    const config = document.getElementById("configSection");
 
-    calculator.scrollIntoView({ behavior: "smooth" });
+    // Smooth scroll zum Config Section
+    config.scrollIntoView({ behavior: "smooth" });
 
+    // Fade-in nach 0,5s
     setTimeout(() => {
         calculator.classList.add("show");
+        config.classList.add("show");
     }, 500);
 });
