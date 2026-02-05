1eed75ca9aca0e127fec06fd36b95aceb2a67fb0
Nicholas Hoops <nicholas.hoops@oktopos.com>
2026-02-05 11:08:22 +0100

    1) PWD_CONTEXT: CryptContext hinzugefügt 2) hash_password(), verify_password() und needs_rehash() hinzugefügt.

--- app/security/passwords.py
+++ app/security/passwords.py
@@ -1,10 +1,15 @@
 # --- Passwort-Helper ---
 #  --- path: app/security/passwords.py ----
 
+from logging import getLogger
 from passlib.context import CryptContext
 from passlib.exc import UnknownHashError, InvalidHashError
-
 from typing import Final
+
+from app.core.exceptions import HashError
+
+
+logger = getLogger(__name__)
 
 PWD_CONTEXT: Final[CryptContext] = CryptContext(
     schemes=["argon2"],
@@ -17,7 +22,7 @@ PWD_CONTEXT: Final[CryptContext] = CryptContext(
 
 def hash_password(password: str) -> str:
     if not isinstance(password, str):
-        raise InvalidHashError("type(password) has to be a string.")
+        raise TypeError("'password' muss ein String sein.")
 
     hashed = PWD_CONTEXT.hash(password)
 
@@ -25,3 +30,19 @@ def hash_password(password: str) -> str:
 
 
 def verify_password(password: str, hashed: str) -> bool:
+    if not isinstance(password, str) or not isinstance(hashed, str) or not hashed:
+        raise TypeError("'password' und 'hashed' müssen Strings sein.")
+
+    try:
+         return PWD_CONTEXT.verify(password, hashed)
+
+    except (UnknownHashError, InvalidHashError):
+        logger.exception("Ein Fehler ist beim Verifizieren des PW-Hashed aufgetreten.")
+        raise HashError()
+
+
+def needs_rehash(hashed: str) -> bool:
+    if not isinstance(hashed, str) or not hashed:
+        raise TypeError("'hashed' muss ein String sein.")
+
+    return PWD_CONTEXT.needs_update(hashed)
