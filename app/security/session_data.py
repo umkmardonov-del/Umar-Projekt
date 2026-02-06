c2938705e5ddbf103908f60204db0bfdab131f5f
Nicholas Hoops <nicholas.hoops@oktopos.com>
2026-02-06 11:18:55 +0100

    SessionData(TypedDict) hinzugefügt

--- app/security/session_data.py
+++ app/security/session_data.py
@@ -1,2 +1,9 @@
-# --- Session Daten für Auth ---
+# --- Session-Daten für Auth ---
 # --- path: /app/security/session_data.py ---
+
+from typing import TypedDict
+
+class SessionData(TypedDict):
+    user_id: str
+    issued_at: int
+    roles: str
