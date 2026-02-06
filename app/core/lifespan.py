fed33679a53f3c8be1de7c0b37853938db24ee9e
Nicholas Hoops <nicholas.hoops@oktopos.com>
2026-02-06 09:36:43 +0100

    Redis in lifespan aufgenommen

--- app/core/lifespan.py
+++ app/core/lifespan.py
@@ -5,11 +5,15 @@ from __future__ import annotations
 
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
+from logging import getLogger
 from sqlalchemy import NullPool, text
 from sqlalchemy.ext.asyncio import create_async_engine
 
+from app.db.engines import ping_redis, close_redis
 from app.core.settings import settings
 
+
+logger = getLogger(__name__)
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -19,7 +23,11 @@ async def lifespan(app: FastAPI):
     )
     async with engine.connect() as conn:
         await conn.scalar(text("SELECT 1"))
-        print("PostgreSQL DB verbunden.")
+        logger.info("PostgreSQL DB verbunden.")
+    await ping_redis()
+    logger.info("Redis DB verbunden.")
     yield
     await engine.dispose()
-    print("Verbindung zu PostgreSQL DB geschlossen.")
+    logger.info("Verbindung zu PostgreSQL DB geschlossen.")
+    await close_redis()
+    logger.info("Verbindung zu Redis DB geschlossen.")
