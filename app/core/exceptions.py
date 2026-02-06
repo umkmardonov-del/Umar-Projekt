bae7fe0507dc16465185d7af2a83d3595e20ab6b
Nicholas Hoops <nicholas.hoops@oktopos.com>
2026-02-06 13:37:42 +0100

    1) JSONDeserializationError() und JSONSerializationError() - jeweils als Subclass(InfraError) hinzugefügt 2) CorruptSessionError(), SessionTimeoutError(), NotPermittedError() und NotAuthorizedError() - alle als Subclass (DomainError) hinzugefügt

--- app/core/exceptions.py
+++ app/core/exceptions.py
@@ -15,3 +15,28 @@ class InfraError(AppError):
 # ------ InfraError Child-Klassen ------
 class HashError(InfraError):
     pass
+
+
+class JSONSerializationError(InfraError):
+    pass
+
+
+class JSONDeserializationError(InfraError):
+    pass
+
+
+# ------- DomainError Child-Klassen -------
+class CorruptSessionError(DomainError):
+    pass
+
+
+class SessionTimeoutError(DomainError):
+    pass
+
+
+class NotPermittedError(DomainError):
+    pass
+
+
+class NotAuthorizedError(DomainError):
+    pass
