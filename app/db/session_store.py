68fa249f5c1563f9ab7687fdddcf3b48b6989c06
Nicholas Hoops <nicholas.hoops@oktopos.com>
2026-02-06 10:53:15 +0100

    login_user() und logout_user() als Redis-Pipeline, wrappt setex/sadd und delete/srem

---
+++ app/db/session_store.py
@@ -0,0 +1,56 @@
+# --- Wrapper um Redis-Methoden und Pipelines ---
+# --- path: /app/db/session_store.py ---
+
+from logging import getLogger
+from json import loads, dumps
+from redis.asyncio import Redis
+from typing import Any, Final
+
+from app.core.exceptions import JSONSerializationError, CorruptSessionError
+
+
+logger = getLogger(__name__)
+
+class SessionStore:
+
+    _KEY_PREFIX: Final[str] = "sess:"
+
+    def __init__(self, client: Redis):
+        self.client: Redis = client
+
+    async def login_user(self,
+                         session_id: str,
+                         session_data: dict[str, Any],
+                         ttl_sec: int) -> None:
+
+        user_id: str = session_data["user_id"]
+        if not session_id or not user_id:
+            logger.error("Variablen 'user_id' und/oder 'session_id' haben falsche Werte!")
+            raise CorruptSessionError()
+
+        try:
+            payload = dumps(session_data, separators=(",", ":"))
+
+        except (ValueError, TypeError, RecursionError):
+            raise JSONSerializationError()
+
+        session_key = f"{self._KEY_PREFIX}{session_id}"
+        set_key = f"user:{user_id}:sessions"
+
+        async with self.client.pipeline() as pipeline:
+            pipeline.setex(session_key, ttl_sec, payload)
+            pipeline.sadd(set_key, session_id)
+            await pipeline.execute()
+
+    async def logout_user(self, session_id: str, user_id: str) -> None:
+        if not session_id or not user_id:
+            logger.error("Variablen 'user_id' und/oder 'session_id' haben falsche Werte!")
+            raise CorruptSessionError()
+
+        session_key = f"{self._KEY_PREFIX}{session_id}"
+        set_key = f"user:{user_id}:sessions"
+
+        async with self.client.pipeline() as pipeline:
+            pipeline.delete(session_key)
+            pipeline.srem(set_key, session_id)
+            await pipeline.execute()
