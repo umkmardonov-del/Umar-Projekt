e7c44e9305a244c46060590592b9e1809f1fe19f
Nicholas Hoops <nicholas.hoops@oktopos.com>
2026-02-09 13:23:45 +0100

    /register und /login-Routes geschrieben

---
+++ app/api/auth/user_api.py
@@ -0,0 +1,39 @@
+# --- Endpunkte für User-Authentifizierung ---
+# --- path: app/api/auth/user_api.py ---
+
+from fastapi import Request, Response, APIRouter, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.core.deps import postgres_dep, session_store_dep
+from app.pydantic_models.auth.user_models import RegisterIn, UserOut, LoginIn
+from app.security.session_store import SessionStore
+from app.service.auth.user_auth_service import register_user_service, create_auth_session_service, login_user_service
+
+
+router = APIRouter(prefix="auth", tags=["auth", "user"])
+
+
+@router.post("/register", response_model=UserOut)
+async def register_endpoint(payload: RegisterIn,
+                            response: Response,
+                            session: AsyncSession = Depends(postgres_dep),
+                            store: SessionStore = Depends(session_store_dep)) -> UserOut:
+
+    user = await register_user_service(session, payload=payload)
+
+    user_dto = await create_auth_session_service(session, store, response=response, user=user)
+
+    return user_dto
+
+
+@router.post("/login", response_model=UserOut)
+async def login_user_endpoint(payload: LoginIn,
+                              response: Response,
+                              session: AsyncSession,
+                              store: SessionStore) -> UserOut:
+
+    user = await login_user_service(session, payload=payload)
+
+    user_dto = await create_auth_session_service(session, store, response=response, user=user)
+
+    return user_dto
