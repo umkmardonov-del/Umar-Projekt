e7bbdba5613180135b544184277846d86609c189
Nicholas Hoops <nicholas.hoops@oktopos.com>
2026-02-09 15:59:54 +0100

    "Base" Import angepasst

--- alembic/env.py
+++ alembic/env.py
@@ -20,7 +20,7 @@ if config.config_file_name is not None:
 
 # add your model's MetaData object here
 # for 'autogenerate' support
-from app.db.orm_models import Base
+from app.db.orm_models.products_orm import Base
 target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
