dae5c3e949cce9397e12949ac8f77eade759ef63
Nicholas Hoops <nicholas.hoops@oktopos.com>
2026-02-09 23:28:40 +0100

    1) request.cookies.get(Name): Name angepasst 2) if store_data is None: Fehler im SessionStore, nicht in get_auth_service()

--- app/security/auth.py
+++ app/security/auth.py
@@ -1,17 +1,17 @@
 # --- Authentifizierungslogik ---
 # --- path: app/security/auth.py ---
 
-from fastapi import Request, Response
+from fastapi import Request, Response, Depends
 from logging import getLogger
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 from typing import Optional
 
-from .session_data import SessionData, is_session_data
-from .session_store import SessionStore
+from app.security.session_data import SessionData, is_session_data
+from app.security.session_store import SessionStore
 from app.db.orm_models.auth_orm import User
 from app.core.exceptions import SessionTimeoutError, CorruptSessionError, NotPermittedError, NotAuthorizedError
-from ..core.settings import settings
+from app.core.settings import settings
 
 logger = getLogger(__name__)
 
@@ -22,17 +22,14 @@ async def get_auth_session(session: AsyncSession,
                            request: Request,
                            response: Response) -> SessionData:
 
-    session_id = request.cookies.get("session_id")
+    session_id = request.cookies.get(settings.SESSION_COOKIE_NAME)
 
     if session_id is None or session_id.strip() == "":
         raise SessionTimeoutError()
 
     store_data = await store.get(session_id)
 
-    if store_data is None:
-        raise SessionTimeoutError()
-
-    elif not is_session_data(store_data):
+    if not is_session_data(store_data):
         logger.error("Falscher type() im store_data.")
         raise CorruptSessionError()
 
