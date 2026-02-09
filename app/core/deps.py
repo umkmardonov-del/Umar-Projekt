61583be7fcf53f9f1e042dd1e5d149c5ae84f402
Nicholas Hoops <nicholas.hoops@oktopos.com>
2026-02-09 15:11:09 +0100

    auth_session_dep() hinzugefügt -> gibt SessionStore-Objekt zurück

--- app/core/deps.py
+++ app/core/deps.py
@@ -3,7 +3,7 @@
 
 from __future__ import annotations
 
-from fastapi import Depends
+from fastapi import Depends, Request, Response
 from redis.asyncio import Redis
 from sqlalchemy.ext.asyncio import AsyncSession
 from typing import AsyncGenerator
@@ -11,9 +11,12 @@ from typing import AsyncGenerator
 from app.db.engines import get_redis_engine
 from app.db.session import AsyncSessionLocal
 from app.security.session_store import SessionStore
+from app.security.auth import get_auth_session
+from app.security.session_data import SessionData
 
 
 async def postgres_dep() -> AsyncGenerator[AsyncSession]:
+
     async with AsyncSessionLocal() as session:
         try:
             yield session
@@ -23,8 +26,17 @@ async def postgres_dep() -> AsyncGenerator[AsyncSession]:
 
 
 async def redis_dep() -> Redis:
+
     return get_redis_engine()
 
 
 async def session_store_dep(client: Redis = Depends(redis_dep)) -> SessionStore:
+
     return SessionStore(client=client)
+
+
+async def auth_session_dep(request: Request, response: Response,
+                           session: AsyncSession = Depends(postgres_dep),
+                           store: SessionStore = Depends(session_store_dep)) -> SessionData:
+
+    return await get_auth_session(session, store, response=response, request=request)
