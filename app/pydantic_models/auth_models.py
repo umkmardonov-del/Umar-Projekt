31b447d4023538cace4964b64df7ae6f183abda9
Nicholas Hoops <nicholas.hoops@oktopos.com>
2026-02-10 15:10:58 +0100

    RoleIn/Out und PermissionIn/Out hinzugefügt, umformatiert

--- app/pydantic_models/auth_models.py
+++ app/pydantic_models/auth_models.py
@@ -5,28 +5,28 @@ from pydantic import Field, EmailStr, SecretStr, BaseModel, StringConstraints, C
 from typing import Annotated
 from uuid import UUID
 
-
-PasswordString = Annotated[SecretStr, StringConstraints(min_length=8, max_length=24)] #TODO Strip whitespace Field Validator
+PasswordString = Annotated[
+    SecretStr, StringConstraints(min_length=8, max_length=24)]  #TODO Strip whitespace Field Validator
 EmailString = Annotated[EmailStr, StringConstraints(max_length=256, strip_whitespace=True)]
 NameString = Annotated[str, StringConstraints(min_length=2, max_length=100, strip_whitespace=True)]
+
 
 # --- Eingabeschemata ---
 
 class RegisterIn(BaseModel):
     email: EmailString = Field(...,
-                            description="E-Mail Adresse.",
-                            examples=["example@example.com"])
+                               description="E-Mail Adresse.",
+                               examples=["example@example.com"])
 
     password: PasswordString = Field(...,
-                                  description="Passwort mit mindestens 8 und höchstens 24 Zeichen.",
-                                  examples=["password", "59iubF!8"])
+                                     description="Passwort mit mindestens 8 und höchstens 24 Zeichen.",
+                                     examples=["password", "59iubF!8"])
 
     name: NameString = Field(...,
-                      description="Vorname, max, 100 Zeichen.")
+                             description="Vorname, max, 100 Zeichen.")
 
     surname: NameString = Field(...,
-                         description="Nachname, max. 100 Zeichen.")
-
+                                description="Nachname, max. 100 Zeichen.")
 
     model_config = ConfigDict(extra="forbid")
 
@@ -37,9 +37,32 @@ class LoginIn(BaseModel):
                             examples=["example@example.com"])
 
     password: PasswordString = Field(...,
-                                  description="Passwort mit mindestens 8 und höchstens 24 Zeichen.",
-                                  examples=["password", "59iubF!8"])
+                                     description="Passwort mit mindestens 8 und höchstens 24 Zeichen.",
+                                     examples=["password", "59iubF!8"])
 
+    model_config = ConfigDict(extra="forbid")
+
+
+class RoleIn(BaseModel):
+    name: str = Field(...,
+                      max_length=20,
+                      description="Name der Rolle.",
+                      examples=["Admin", "Maintainer"])
+
+    description: str = Field(max_length=256,
+                             description="Rollenbeschreibung.")
+
+    model_config = ConfigDict(extra="forbid")
+
+
+class PermissionIn(BaseModel):
+    code: str = Field(...,
+                      max_length=100,
+                      description="Permission Code.",
+                      examples=["user:read", "admin:read"])
+
+    description: str = Field(max_length=256,
+                             description="Berechtigungsbeschreibung.")
 
     model_config = ConfigDict(extra="forbid")
 
@@ -51,17 +74,24 @@ class UserOut(BaseModel):
                      description="Automatisch generierte User ID.")
 
     email: EmailString = Field(...,
-                            description="E-Mail Adresse.",
-                            examples=["example@example.com"])
+                               description="E-Mail Adresse.",
+                               examples=["example@example.com"])
 
     name: NameString = Field(...,
-                      description="Vorname, max, 100 Zeichen.")
+                             description="Vorname, max, 100 Zeichen.")
 
     surname: NameString = Field(...,
-                         description="Nachname, max. 100 Zeichen.")
+                                description="Nachname, max. 100 Zeichen.")
+
+    model_config = ConfigDict(extra="forbid", from_attributes=True)
 
 
-    model_config = ConfigDict(extra="forbid")
+class RoleOut(BaseModel):
+    pass
+
+
+class PermissionOut(BaseModel):
+    pass
 
 
 class Me(BaseModel):
