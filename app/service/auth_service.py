7621c2c33464d85726a8a95b0cbae600f6e8fb62
Nicholas Hoops <nicholas.hoops@oktopos.com>
2026-02-10 15:04:04 +0100

    Variablen umbenannt

--- app/service/auth_service.py
+++ app/service/auth_service.py
@@ -9,14 +9,18 @@ from zoneinfo import ZoneInfo
 
 from app.core.settings import settings
 from app.db.orm_models.auth_orm import User
-from app.db.repositories.auth_repository import get_user_by_email, register_user, get_permission_codes_by_id
-from app.core.exceptions import EmailAlreadyRegisteredError, InvalidCredentialsError
-from app.pydantic_models.auth_models import RegisterIn, UserOut, LoginIn
+from app.db.repositories.auth_repository import (get_user_by_email, register_user, get_permission_codes_by_id,
+                                                 create_role, get_role_by_name, get_permission_by_code,
+                                                 create_permission)
+from app.core.exceptions import (EmailAlreadyRegisteredError, InvalidCredentialsError, RoleAlreadyExistsError,
+                                 PermissionAlreadyExistsError)
+from app.pydantic_models.auth_models import RegisterIn, UserOut, LoginIn, RoleIn, RoleOut, PermissionIn, PermissionOut
 from app.security.passwords import hash_password, needs_rehash, verify_password, generate_secret_token
 from app.security.session_data import SessionData
 from app.security.session_store import SessionStore
 
 
+# --- User Funktionen ---
 async def register_user_service(session: AsyncSession,
                                 *,
                                 payload: RegisterIn) -> User:
@@ -26,8 +30,8 @@ async def register_user_service(session: AsyncSession,
     email = payload.email
     password = payload.password.get_secret_value()
 
-    assigned = await get_user_by_email(session, email=email)
-    if assigned:
+    exists = await get_user_by_email(session, email=email)
+    if exists:
         raise EmailAlreadyRegisteredError()
 
     pw_hash = hash_password(password)
@@ -60,8 +64,6 @@ async def login_user_service(session: AsyncSession,
 
         user_dto.pw_hash = new_pw_hash
 
-        session.add(user_dto.pw_hash)
-
         await session.flush()
         await session.refresh(user_dto)
 
@@ -72,11 +74,11 @@ async def create_auth_session_service(session: AsyncSession,
                                       store: SessionStore,
                                       *,
                                       response: Response,
-                                      user: User) -> UserOut:
+                                      user_orm: User) -> UserOut:
 
-    data: SessionData = {"user_id": str(user.id),
+    data: SessionData = {"user_id": str(user_orm.id),
                          "issued_at": int(datetime.now(tz=ZoneInfo("Europe/Berlin")).timestamp()),
-                         "permissions": await get_permission_codes_by_id(session, ident=user.id)}
+                         "permissions": await get_permission_codes_by_id(session, ident=user_orm.id)}
 
     await session.commit()
 
@@ -94,7 +96,7 @@ async def create_auth_session_service(session: AsyncSession,
                         httponly=True,
                         samesite=settings.COOKIE_SAMESITE)
 
-    user_dto = UserOut(id=user.id, email=user.email, name=user.name, surname=user.surname)
+    user_dto = UserOut.model_validate(user_orm)
 
     return user_dto
 
