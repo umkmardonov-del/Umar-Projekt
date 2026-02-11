4e8c169819aae96a023a181bf9586a193812b305
Nicholas Hoops <nicholas.hoops@oktopos.com>
2026-02-11 15:20:25 +0100

    auth_admin_router eingehängt

--- app/main.py
+++ app/main.py
@@ -8,7 +8,7 @@ from fastapi.staticfiles import StaticFiles
 from pathlib import Path
 from uvicorn import run
 
-from app.api.auth_api import user_router as auth_user_router
+from app.api.auth_api import user_router as auth_user_router, admin_router as auth_admin_router
 from app.api.products_api import admin_router as product_admin_router, user_router as product_user_router
 from app.core.lifespan import lifespan
 from app.core.logging_config import configure_logging
@@ -37,6 +37,7 @@ app.include_router(product_admin_router)
 
 # --- Auth Routers ---
 app.include_router(auth_user_router)
+app.include_router(auth_admin_router)
 
 
 if __name__ == "__main__":
