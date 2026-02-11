cc87badbb28d487b77128a0e9bc5df24262e87cc
Nicholas Hoops <nicholas.hoops@oktopos.com>
2026-02-11 09:18:37 +0100

    1) cast email: EmailStr -> email: object -> email: str 2) register_user_service() gibt Daten anders an register_user() weiter 3) Verantwortung der String-Validierung (strip(), lower()) in Pydantic-Modelle ausgelagert 4) Schönheitsverbesserungen: bessere Lesbarkeit

--- app/service/auth_service.py
+++ app/service/auth_service.py
@@ -5,6 +5,7 @@ from datetime import datetime
 from fastapi import Request, Response
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy.ext.asyncio import AsyncSession
+from typing import cast
 from zoneinfo import ZoneInfo
 
 from app.core.settings import settings
@@ -22,14 +23,14 @@ from app.security.session_store import SessionStore
 
 
 # --- User Funktionen ---
-async def register_user_service(session: AsyncSession,
-                                *,
-                                payload: RegisterIn) -> User:
+async def register_user_service(session: AsyncSession, *, payload: RegisterIn) -> User:
 
-    name = payload.name.strip()
-    surname = payload.surname.strip()
-    email = payload.email
     password = payload.password.get_secret_value()
+    email = payload.email
+
+    # --- Typechecker ---
+    email = cast(object, email)
+    email = cast(str, email)
 
     exists = await get_user_by_email(session, email=email)
     if exists:
@@ -38,7 +39,7 @@ async def register_user_service(session: AsyncSession,
     pw_hash = hash_password(password)
 
     try:
-        user = await register_user(session, name=name, surname=surname, email=email, pw_hash=pw_hash)
+        user = await register_user(session, payload=payload, email=email, pw_hash=pw_hash)
 
     except IntegrityError:
         raise EmailAlreadyRegisteredError()
@@ -46,11 +47,15 @@ async def register_user_service(session: AsyncSession,
     return user
 
 
-async def login_user_service(session: AsyncSession,
-                             *,
-                             payload: LoginIn) -> User:
+async def login_user_service(session: AsyncSession, *, payload: LoginIn) -> User:
 
-    user_dto = await get_user_by_email(session, email=payload.email)
+    email = payload.email
+
+    # --- Typechecker ---
+    email = cast(object, email)
+    email = cast(str, email)
+
+    user_dto = await get_user_by_email(session, email=email)
     if not user_dto:
         raise InvalidCredentialsError()
 
@@ -71,11 +76,8 @@ async def login_user_service(session: AsyncSession,
     return user_dto
 
 
-async def create_auth_session_service(session: AsyncSession,
-                                      store: SessionStore,
-                                      *,
-                                      response: Response,
-                                      user_orm: User) -> UserOut:
+async def create_auth_session_service(session: AsyncSession, store: SessionStore,
+                                      *, response: Response, user_orm: User) -> UserOut:
 
     data: SessionData = {"user_id": str(user_orm.id),
                          "issued_at": int(datetime.now(tz=ZoneInfo("Europe/Berlin")).timestamp()),
@@ -102,11 +104,8 @@ async def create_auth_session_service(session: AsyncSession,
     return user_dto
 
 
-async def logout_user_service(store: SessionStore,
-                              data: SessionData,
-                              *,
-                              request: Request,
-                              response: Response) -> None:
+async def logout_user_service(store: SessionStore, data: SessionData,
+                              *, request: Request, response: Response) -> None:
 
     session_id = request.cookies.get(settings.SESSION_COOKIE_NAME)
     user_id = data["user_id"]
@@ -119,8 +118,8 @@ async def logout_user_service(store: SessionStore,
 # --- Admin Funktionen ---
 async def create_role_service(session: AsyncSession, payload: RoleIn) -> RoleOut:
 
-    name = payload.name.strip()
-    description = payload.description.strip()
+    name = payload.name
+    description = payload.description
 
     exists = await get_role_by_name(session, name)
     if exists:
@@ -135,8 +134,8 @@ async def create_role_service(session: AsyncSession, payload: RoleIn) -> RoleOut
 
 async def create_permission_service(session: AsyncSession, payload: PermissionIn) -> PermissionOut:
 
-    code = payload.code.strip()
-    description = payload.description.strip()
+    code = payload.code
+    description = payload.description
 
     exists = await get_permission_by_code(session, code)
     if exists:
@@ -151,8 +150,12 @@ async def create_permission_service(session: AsyncSession, payload: PermissionIn
 
 async def create_user_role_service(session: AsyncSession, payload: UserRoleIn) -> UserRoleOut:
 
-    email = payload.email.strip()
-    name = payload.role_name.strip()
+    email = payload.email
+    name = payload.role_name
+
+    # --- Typechecker ---
+    email = cast(object, email)
+    email = cast(str, email)
 
     user = await get_user_by_email(session, email)
     role = await get_role_by_name(session, name)
@@ -166,8 +169,8 @@ async def create_user_role_service(session: AsyncSession, payload: UserRoleIn) -
 
 async def create_role_permission_service(session: AsyncSession, payload: RolePermissionIn) -> RolePermissionOut:
 
-    name = payload.role_name.strip()
-    code = payload.permission_code.strip()
+    name = payload.role_name
+    code = payload.permission_code
 
     role = await get_role_by_name(session, name)
     permission = await get_permission_by_code(session, code)
